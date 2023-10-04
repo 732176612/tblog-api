350ea53a4658e179c3efa624d1cdc2c1ff37bfca
FalseEndLess <732176612@qq.com>
2023-10-04 19:41:50 +0800

    update

--- Tblog.RabbitMQ/Base/RabbitMQFactory.cs
+++ Tblog.RabbitMQ/Base/RabbitMQFactory.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using TBlog.Common;
+using TBlog.IRepository;
 
 namespace Tblog.RabbitMQ
 {
@@ -11,9 +12,10 @@ namespace Tblog.RabbitMQ
             service.AddSingleton(sp =>
             {
                 var connection = sp.GetRequiredService<IRabbitMQConnection>();
+                var redis = sp.GetRequiredService<IRedisRepository>();
                 var logger = sp.GetRequiredService<ILogger<TestQueue>>();
                 var retryCount = ApiConfig.RabbitMQ.RetryCount;
-                var queue = new TestQueue(connection, logger, retryCount: retryCount);
+                var queue = new TestQueue(connection, logger, redis, retryCount: retryCount);
                 return queue;
             });
         }
