27eb87f8ea1b8351046ccfc782e4be1bef6d485f
FalseEndLess <732176612@qq.com>
2024-12-01 18:37:38 +0800

    使用顶级语句写法

--- TBlog.Api/Program.cs
+++ TBlog.Api/Program.cs
@@ -1,31 +1,175 @@
-using Autofac.Extensions.DependencyInjection;
+global using TBlog.Extensions;
+global using TBlog.IService;
+global using TBlog.Model;
+global using TBlog.Repository;
+global using System;
+global using TBlog.Common;
+using Autofac;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using SqlSugar;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.Extensions.FileProviders;
 using System.IO;
-using System;
-namespace TBlog.Api
+using System.Net;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using Microsoft.Extensions.Primitives;
+using TBlog.RabbitMQ;
+using AspNetCoreRateLimit;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using TBlog.Api;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host
+.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+.ConfigureContainer<ContainerBuilder>(builder =>
 {
-    public class Program
+    builder.RegisterModule(new AutofacModuleRegister());
+    builder.RegisterModule<AutofacPropertityModuleReg>();
+    builder.RegisterBuildCallback(c =>
     {
-        public static void Main(string[] args)
-        {
-            Host.CreateDefaultBuilder(args)
-            .UseServiceProviderFactory(new AutofacServiceProviderFactory())
-            .ConfigureWebHostDefaults(webBulider =>
-            {
-                webBulider
-                .UseStartup<Startup>()
-                .ConfigureLogging((hostingContext, builder) =>
-                {
-                    builder.AddFilter("System", LogLevel.Error);
-                    builder.AddFilter("Microsoft", LogLevel.Error);
-                    builder.SetMinimumLevel(LogLevel.Error);
-                    builder.AddLog4Net(Path.Combine(Directory.GetCurrentDirectory(), "Log4net.config"));
-                });
-            })
-            .Build().Run();
-        }
-    }
+        ContainerHelper.RegisterContainer(c as IContainer);
+    });
+})
+.ConfigureLogging((hostingContext, builder) =>
+{
+    builder.AddFilter("System", LogLevel.Error);
+    builder.AddFilter("Microsoft", LogLevel.Error);
+    builder.SetMinimumLevel(LogLevel.Error);
+    builder.AddLog4Net(Path.Combine(Directory.GetCurrentDirectory(), "Log4net.config"));
+});
+builder.Services.AddSingleton(new ApiConfig(builder.Configuration, AppDomain.CurrentDomain.BaseDirectory));
+ChangeToken.OnChange(() => ((IConfiguration)builder.Configuration).GetReloadToken(), () =>
+{
+    builder.Services.AddSingleton(new ApiConfig(builder.Configuration, AppDomain.CurrentDomain.BaseDirectory));
+});
+builder.Services.AddSingleton(new LogLock(AppDomain.CurrentDomain.BaseDirectory));
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+builder.Services.AddMemoryCacheSetup();
+builder.Services.AddRedisCacheSetup();
+builder.Services.AddElasticsearchSetup();
+builder.Services.AddMongoDBSetup();
+builder.Services.AddCorsSetup();
+builder.Services.AddMiniProfilerSetup();
+builder.Services.AddSwaggerSetup();
+builder.Services.AddHttpContextSetup();
+
+//builder.Services.AddRabbitMQSetup();
+
+// 授权+认证 (jwt or ids4)
+builder.Services.AddAuthorizationSetup();
+builder.Services.AddAuthentication_Ids4Setup();
+builder.Services.AddAuthentication_JWTSetup();
+
+builder.Services.AddIpPolicyRateLimitSetup(builder.Configuration);
+
+builder.Services.AddSignalR().AddNewtonsoftJsonProtocol();
+
+builder.Services.Configure<KestrelServerOptions>(x => x.AllowSynchronousIO = true)
+        .Configure<IISServerOptions>(x => x.AllowSynchronousIO = true);
+
+builder.Services.AddControllers(o =>
+{
+    o.Filters.Add<GlobalExceptionsFilter>();
+    o.InputFormatters.Add(new PlainTextInputFormatter());
+    o.InputFormatters.Add(new CustInputFormatter());
+})
+.AddNewtonsoftJson(options =>
+{
+    //忽略循环引用
+    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+    //不使用驼峰样式的key
+    options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+    //设置本地时间而非UTC时间
+    options.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Local;
+});
+builder.Services.Replace(ServiceDescriptor.Transient<IControllerActivator, ServiceBasedControllerActivator>());
+builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+//支持编码大全 例如:支持 System.Text.Encoding.GetEncoding("GB2312")  System.Text.Encoding.GetEncoding("GB18030") 
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+var app = builder.Build();
+app.UseIpLimitMildd();
+
+app.UseHttpLog();
+
+app.UseSignalRSendMildd();
+
+app.UseAllServicesMildd(builder.Services);
+
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseSwaggerMildd();
+
+app.UseCorsMildd();
+
+app.UseCookiePolicy();
+
+app.UseStatusCodePages();
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.UseMiniProfilerMildd();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+
+    endpoints.MapHub<ChatHub>("/api2/chatHub");
+
+    endpoints.MapFallback(async context =>
+    {
+        if (context.Request.Path.StartsWithSegments("/view", StringComparison.Ordinal))
+        {
+            var html = await File.ReadAllTextAsync(Path.Combine(builder.Environment.WebRootPath, "./view/index.html"));
+            context.Response.ContentType = "text/html";
+            await context.Response.WriteAsync(html, Encoding.UTF8);
+        }
+        else
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+        }
+    });
+});
+
+app.UseStaticFiles();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.WebRootPath, "./view")),
+    RequestPath = "/view"
+});
+
+SqlSugarDBSeed.SeedAsync();
+
+app.UseConsulMildd(app.Lifetime);
+
+//if (env.IsDevelopment() == false)
+//{
+//    app.Applicationbuilder.Services.GetService<TestQueue>().Start();
+//}
+
+app.Run();
